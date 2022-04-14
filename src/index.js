f1d8ab7814a1b50b8c2515925ca4833017b3c0d7
Ajuka David <ajuka.zephiniah@gmail.com>
2022-04-14 18:42:02 +0200

    indepth analysis of DOM  node structure

--- src/index.js
+++ src/index.js
@@ -2,4 +2,43 @@ import '../assets/css/style.css';
 
 const app = document.getElementById('app');
 
-app.innerHTML = ``;
+app.innerHTML = `<h1>Javascript DOM </h1>`;
+//html
+
+console.log(document.documentElement)
+console.dir(document.documentElement)
+
+//head
+console.dir(document.head)
+
+//retrieve constructor name
+console.log(document.body.constructor.name)
+
+//looking at the prototype chain
+console.log(document.body instanceof HTMLBodyElement)
+console.log(document.body instanceof Element)
+console.log(document.body instanceof Node)
+console.log(document.body instanceof EventTarget)
+
+
+// Node types
+/*
+Element
+Attribute
+Text
+CDATASection
+EntityReference
+Entity
+ProcessingInstruction
+Comment
+Doument
+DocumentType
+DocumentFragment
+Notation
+ */
+
+console.log(document.body.nodeType)
+console.log(document.nodeType)
+
+console.log(document.body.nodeName)
+console.log(document.body.tagName)
