95f0fcf94f89a237090fb33cafa5de8a10415142
Ajuka David <ajuka.zephiniah@gmail.com>
2022-04-14 19:20:43 +0200

    creating DOM elements(div. text and comments) by appending them

--- src/index.js
+++ src/index.js
@@ -2,43 +2,55 @@ import '../assets/css/style.css';
 
 const app = document.getElementById('app');
 
-app.innerHTML = `<h1>Javascript DOM </h1>`;
-//html
 
-console.log(document.documentElement)
-console.dir(document.documentElement)
+const div = document.createElement('div')
+const text = document.createTextNode('DOM!')
+const comment = document.createComment('No comment')
 
-//head
-console.dir(document.head)
+div.append(comment)
+div.append(text)
+app.append(div)
 
-//retrieve constructor name
-console.log(document.body.constructor.name)
-
-//looking at the prototype chain
-console.log(document.body instanceof HTMLBodyElement)
-console.log(document.body instanceof Element)
-console.log(document.body instanceof Node)
-console.log(document.body instanceof EventTarget)
+console.log(app, div)
 
 
-// Node types
-/*
-Element
-Attribute
-Text
-CDATASection
-EntityReference
-Entity
-ProcessingInstruction
-Comment
-Doument
-DocumentType
-DocumentFragment
-Notation
- */
+// app.innerHTML = `<h1>Javascript DOM </h1>`;
+// //html
 
-console.log(document.body.nodeType)
-console.log(document.nodeType)
+// console.log(document.documentElement)
+// console.dir(document.documentElement)
 
-console.log(document.body.nodeName)
-console.log(document.body.tagName)
+// //head
+// console.dir(document.head)
+
+// //retrieve constructor name
+// console.log(document.body.constructor.name)
+
+// //looking at the prototype chain
+// console.log(document.body instanceof HTMLBodyElement)
+// console.log(document.body instanceof Element)
+// console.log(document.body instanceof Node)
+// console.log(document.body instanceof EventTarget)
+
+
+// // Node types
+// /*
+// Element
+// Attribute
+// Text
+// CDATASection
+// EntityReference
+// Entity
+// ProcessingInstruction
+// Comment
+// Doument
+// DocumentType
+// DocumentFragment
+// Notation
+//  */
+
+// console.log(document.body.nodeType)
+// console.log(document.nodeType)
+
+// console.log(document.body.nodeName)
+// console.log(document.body.tagName)
