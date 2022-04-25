4b17f4c40fa428d687ff1909d4b71250ab3535d2
Ajuka David <ajuka.zephiniah@gmail.com>
2022-04-25 03:52:04 +0200

    working with checkboxes

--- src/index.js
+++ src/index.js
@@ -5,40 +5,17 @@ const app = document.getElementById("app");
 app.innerHTML = `
 <h1>Javascript DOM</h1>
 <form name="example">
-<div class="container">
-        <label>
-        blue
-        <input type="radio" name="color" value="blue" checked>
-        </label>
-        <label>
-        Red
-        <input type="radio" name="color" value="red">
-        </label>
-        <label>
-        Green
-        <input type="radio" name="color" value="green">
-        </label>
-        </div>
+<label>
+Accept Marketing
+<input type="checkbox" name="marketing">
+
+</label>
 </form>
 `;
 
+const form = document.forms.example
+const checkbox = form.elements.marketing
 
+console.dir(checkbox)
 
-const form  = document.forms.example
-const radios = [...form.elements.color]
-
-//properies on radio buttons
-radios.forEach(radio => {
-    console.log(radio.value)
-    console.log(radio.checked)
-
-})
-
-const container = form.querySelector('.container')
-container.addEventListener('change', () => {
-    console.log(form.elements.color.value)
-})
-// const checked = radios.find(radio => radio.checked)
-// console.log(checked)
-// console.log(radios)
-
+checkbox.select()
