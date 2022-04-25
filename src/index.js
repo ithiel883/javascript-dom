7c77a6776f85e35ae884ed954ade802bc6eed842
Ajuka David <ajuka.zephiniah@gmail.com>
2022-04-25 04:21:48 +0200

    beginning of vanilla todo list

--- src/index.js
+++ src/index.js
@@ -3,21 +3,25 @@ import "../assets/css/style.css";
 const app = document.getElementById("app");
 
 app.innerHTML = `
-<h1>Javascript DOM</h1>
-<form name="example">
-
-<select name="drink">
-<option value="">Select your drink</option>
-<option value="lemonade" selected>Lemondade</option>
-<option value="cola">Cola</option>
-<option value="water">Water</option>
-
-</select>
+<div class="todos>
+<div class="todos-header">
+<h3 class="todos-title">Todo List</h3>
+<div>
+<p>You have <span class="todos-count"></span> Items  </p>
+<button type"button" class="todos-clear" style="display: none;">
+Clear completed
+</button>
+</div>
+</div>
+<form class="todos-form" name="todos">
+<input type="text" placeholder="what's next ?" name="todo" />
 </form>
+<ul class="todos-list>
+</ul>
+</div>
+
+
 `;
 
-const form = document.forms.example;
-const select = form.elements.drink
 
-console.log(select.value)
 
